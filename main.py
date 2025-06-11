181b9cce5f6b6675bb8b867f5607c74474842223
Arman singh <armansin667@gmail.com>
2025-06-11 23:24:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+
+city=input("Enter city name: ")
+key="331062d65adeedfdd772ea68a6d73828"
+
+url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric"
+
+response=requests.get(url)
+if response.status_code == 200:# Check if the request was successful
+    data=response.json()
+
+    print(f"City: {data['name']}")
+    print(f"Temperature: {data['main']['temp']}°C")
+    print(f"Feels Like:{data['main']['feels_like']}°C")
+    print(f"Weather: {data['weather'][0]['description']}")
+else:
+    print("Error:", response.status_code)
+    print("City not found or API request failed.")
+
+    
